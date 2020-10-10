fd66bdb688b19a1328ec0efda80faf49b7cf4de1
Bill Torode <wmtorode@gmail.com>
2020-10-09 22:42:12 -0400

    initial commit

---
+++ DCECore.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+using System.Reflection;
+using IRBTModUtils.Logging;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace DropCostsEnchanced
+{
+    public class DCECore
+    {
+        internal static DeferringLogger modLog;
+        //internal static Settings settings;
+        internal static string modDir;
+
+        public static void Init(string modDirectory, string settingsJSON)
+        {
+            
+            modDir = modDirectory;
+
+            modLog = new DeferringLogger(modDirectory, "drop_cost_enhanced", "DCE", false, false);
+
+            try
+            {
+                using (StreamReader reader = new StreamReader($"{modDir}/settings.json"))
+                {
+                    string jdata = reader.ReadToEnd();
+                    //settings = JsonConvert.DeserializeObject<Settings>(jdata);
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                modLog.Error?.Write(ex);
+            }
+
+            var harmony = HarmonyInstance.Create("ca.jwolf.DropCostsEnhanced");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+        }
+    }
+}
